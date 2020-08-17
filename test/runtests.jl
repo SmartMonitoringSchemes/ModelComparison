f707c45a4e7ba728f5f9a705689bb123e450d38d
Maxime Mouchet <max@maxmouchet.com>
2020-08-17 18:22:06 +0200

    Test AIC penalty

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Test
     data = rand(1000)
     
     @test fit(FiniteMixtureModel{Normal}, data, n_components = 5, n_init = 3) isa MixtureModel
-    @test fit(FiniteMixtureModel{Normal}, data, 1:5, BIC) isa MixtureModel
+    @test fit(FiniteMixtureModel{Normal}, data, 1:5, AIC) isa MixtureModel
     
     @test fit(FiniteHMM{Normal}, data, n_components = 5, estimator = fit_map) isa HMM
     @test fit(FiniteHMM{Normal}, data, 1:5, BIC, estimator = fit_map) isa HMM
